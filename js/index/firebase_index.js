4791e43911e8aff40e1cfdb1c3dd4f18f167be54
Novachocolat <lysandre.pb@gmail.com>
2025-09-22 20:19:31 +0200

    Correctifs :
    - CSS
    - JS Firebase

--- js/index/firebase_index.js
+++ js/index/firebase_index.js
@@ -18,7 +18,7 @@ const clientfirebaseConfig = {
   appId: "YOUR_APP_ID"
 };
 
-firebase.initializeApp(developperfirebaseConfig);
+firebase.initializeApp(clientfirebaseConfig);
 const db = firebase.firestore();
 
 // Fonction pour charger le pop-up depuis Firestore
